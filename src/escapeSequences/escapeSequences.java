59363c4d8be619b343564e3412ff3de2de9c33a3
Jordan Lamar <jordanlamar153@gmail.com>
2022-09-30 13:50:41 -0400

    first commit

---
+++ src/escapeSequences/escapeSequences.java
@@ -0,0 +1,12 @@
+package escapeSequences;
+
+public class escapeSequences {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Dog");
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+	}
+
+}
